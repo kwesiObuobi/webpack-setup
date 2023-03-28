255399d9b59d39266ce302a64dce52e27ef24061
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-03-28 07:41:25 +0000

    Add css loading to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,4 +14,12 @@ module.exports = {
     clean: true,
   },
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
